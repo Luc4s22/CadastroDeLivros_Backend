d35bbdb56eff09f1d6c5f5a5563df10be034a26a
Lucas Mendes <lucas.silva0020015704@edu.pe.senac.br>
2026-04-25 18:12:05 -0300

    Update livros.js

--- models/livros.js
+++ models/livros.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 const livrosSchema = new mongoose.Schema(
   {
     nome: { type: String, required: true, trim: true },
-    data: { type: String, required: true, trim: true },
+    data: { type: String, required: true },
     editora: { type: String, required: true, trim: true }
   },
-  {
-    timestamps: true
-  }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('livros', livrosSchema);
